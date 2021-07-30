8075e8ed95506dc566dd02ecad17ddea3e49c5cb
taschunina <taschunina@yandex.ru>
2021-07-30 22:44:52 +0300

    feat: add App.tsx (test)

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <p>Content</p>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
