dac7428fa0cfabf78c016b02d6b0241ac2164876
taschunina <taschunina@yandex.ru>
2021-08-01 22:44:14 +0300

    chore: add style for Layout (test, Layout.css)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+
+import './Layout.css';
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      {children}
+      <section>{children}</section>
       <Footer />
     </>
   );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default Layout;
