f8f5f045e910c1df274b598d5c588009d457a9ec
taschunina <taschunina@yandex.ru>
2021-08-07 17:33:07 +0300

    feat: added styles

---
+++ src/components/Layout/Layout.styles.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const LayoutStyled = styled.div`
+  margin: 0 auto;
+  background-color: #fff;
+  color: rgba(51, 51, 102, 1);
+  max-width: 1440px;
+  font-family: YS Text;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 22px;
+  line-height: 28px;
+`;
+
+export default LayoutStyled;
