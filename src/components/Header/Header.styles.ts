b35e714d0271aec818221638029214a80ef22ac6
taschunina <taschunina@yandex.ru>
2021-08-10 23:18:41 +0300

    chore: add header-component (test)

---
+++ src/components/Header/Header.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const HeaderStyles = styled.header`
+  margin-top: 0;
+  margin-bottom: 0;
+  height: 100px;
+  width: 800px;
+  font-size: 38px;
+  background-color: orchid;
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,6 @@
 import React from 'react';
+import * as Styles from './Header.styles';
 
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
+export const Header = () => {
+  return <Styles.HeaderStyles>Congratulations! It is the best Header</Styles.HeaderStyles>;
 };
-const headingAccentStyles = {
-  color: '#663399',
-};
-
-const Header = () => {
-  return (
-    <h1 style={headingStyles}>
-      Congratulations
-      <br />
-      <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-      <span role="img" aria-label="Party popper emojis">
-        🎉🎉🎉
-      </span>
-    </h1>
-  );
-};
-
-export default Header;
