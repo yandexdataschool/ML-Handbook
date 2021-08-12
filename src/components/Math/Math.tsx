57dd31755e088643d280134de17038458ec709b3
khizrim <kh.makhmudov@me.com>
2021-08-12 07:41:56 +0300

    feat: add ability to choose inline or block mode

--- src/components/Math/Math.tsx
+++ src/components/Math/Math.tsx
@@ -3,8 +3,17 @@ import React from 'react';
 import 'katex/dist/katex.min.css';
 import TeX from '@matejmazur/react-katex';
 
-const Math = ({ children }: { children: React.Component }) => {
-  return <TeX block>{String.raw`${children.props.children}`}</TeX>;
+interface MathProps {
+  block: boolean;
+  children: React.Component;
+}
+
+const Math = ({ block, children }: MathProps) => {
+  return block ? (
+    <TeX block>{String.raw`${children.props.children}`}</TeX>
+  ) : (
+    <TeX>{String.raw`${children.props.children}`}</TeX>
+  );
 };
 
 export default Math;
