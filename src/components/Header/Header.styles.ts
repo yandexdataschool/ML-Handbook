85f98f6dcb5be14f99ab6a0ef4d246ce438540ca
taschunina <taschunina@yandex.ru>
2021-08-15 22:55:18 +0300

    chore: edited header

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,10 +1,68 @@
 import styled from 'styled-components';
+import backVectorImage from './images/Vector.png';
+import findImage from './images/Find.png';
 
-export const Header = styled.header`
+const Header = styled.header`
   margin-top: 0;
   margin-bottom: 0;
-  height: 100px;
-  width: 800px;
-  font-size: 38px;
-  background-color: orchid;
+  max-width: 1440px;
+  height: 52px;
+  display: flex;
 `;
+
+const Title = styled.h1`
+  max-width: 720px;
+  height: 24px;
+  margin-block: 0;
+  margin: auto 0;
+  font-family: 'Yandex Sans Text';
+  font-weight: 300;
+  font-style: normal;
+  font-size: 20px;
+`;
+
+const BackToMain = styled.div`
+  width: 280px;
+  height: 24px;
+  margin: auto 20px;
+  display: flex;
+`;
+
+const BackVector = styled.image`
+  width: 14px;
+  height: 14px;
+  margin: auto 0;
+  background-repeat: no-repeat;
+  background-image: url(${backVectorImage});
+`;
+
+const BackTitle = styled.h2`
+  width: 250px;
+  height: 24px;
+  margin-left: 6px;
+  margin-block: 0;
+  font-family: 'Yandex Sans Text';
+  font-weight: 500;
+  font-style: normal;
+  font-size: 20px;
+  color: rgba(59, 72, 86, 1);
+`;
+
+const WBmode = styled.h2`
+  width: 236px;
+  height: 21px;
+  font-family: 'Yandex Sans Text';
+  font-weight: 500;
+  font-style: normal;
+  font-size: 14px;
+  color: rgba(59, 72, 86, 1);
+`;
+
+const Find = styled.image`
+  width: 32px;
+  height: 32px;
+  margin: auto;
+  background-image: url(${findImage});
+`;
+
+export { Header, Title, BackToMain, BackVector, BackTitle, WBmode, Find };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import * as Styles from './Header.styles';
 
+// export const Header = () => {
+//   return <Styles.Header>Congratulations! It is the best Header</Styles.Header>;
+// };
+
+import { Title, BackToMain, BackVector, BackTitle, WBmode, Find } from './Header.styles';
+
 export const Header = () => {
-  return <Styles.Header>Congratulations! It is the best Header</Styles.Header>;
+  return (
+    <Styles.Header>
+      <BackToMain>
+        <BackVector />
+        <BackTitle>ML-учебник</BackTitle>
+      </BackToMain>
+      <Title>Вероятностный подход в ML</Title>
+      <WBmode />
+      <Find />
+    </Styles.Header>
+  );
 };
