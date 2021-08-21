2d6586cb18a0229ae7cb6e862fb1ba0c829fb4ba
taschunina <taschunina@yandex.ru>
2021-08-21 15:41:45 +0300

    chore: hide the sidebar on the small width

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
 export const Sidebar = styled.section`
+  display: none;
   width: 300px;
   height: 952px;
   color: aqua;
   font-size: 15px;
   border: 1px solid aquamarine;
+
+  @media screen and (min-width: 1140px) {
+    display: block;
+  }
 `;
