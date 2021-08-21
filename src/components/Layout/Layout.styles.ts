3e73b9918357ffc8b0fce5d6043907760e7b8999
taschunina <taschunina@yandex.ru>
2021-08-21 15:43:14 +0300

    chore: added max-width instead of width for layout

--- src/components/Layout/Layout.styles.ts
+++ src/components/Layout/Layout.styles.ts
@@ -1,21 +1,21 @@
 import styled from 'styled-components';
 
-export const Main = styled.main`
+export const Page = styled.div`
+  max-width: 1440px;
   margin: 0 auto;
   background-color: #fff;
   color: rgba(51, 51, 102, 1);
-  width: 1440px;
+`;
+
+export const Main = styled.main`
+  max-width: 1140px;
+  margin: 0 auto;
+  background-color: #fff;
+  color: rgba(51, 51, 102, 1);
   font-family: YS Text;
   font-style: normal;
   font-weight: normal;
   font-size: 22px;
   line-height: 28px;
   display: flex;
-`;
-
-export const Page = styled.div`
-  margin: 0 auto;
-  background-color: #fff;
-  color: rgba(51, 51, 102, 1);
-  width: 1440px;
 `;
