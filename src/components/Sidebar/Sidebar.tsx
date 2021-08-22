c88ee84c5a0d829cd8bf452af6d89318b4e4af18
taschunina <taschunina@yandex.ru>
2021-08-22 14:17:54 +0300

    chore: styles for sidebar (test)

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -6,8 +6,7 @@ export const Sidebar = styled.section`
   @media screen and (min-width: 1140px) {
     display: block;
     min-width: 300px;
-    color: aqua;
     font-size: 15px;
-    border: 1px solid aquamarine;
+    box-shadow: inset -5px 0 30px -20px rgba(0, 0, 0, 0.06);
   }
 `;
