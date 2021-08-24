a9c393f7cfacd6262bf9dbcf0ec561621abe1eb5
taschunina <taschunina@yandex.ru>
2021-08-24 20:03:28 +0300

    fix: removed mdx-provider to the layout

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MDXProvider } from '@mdx-js/react';
+
 import { graphql } from 'gatsby';
 import * as Styles from './Content.styles';
 import {
@@ -18,49 +18,45 @@ type Props = {
   children: React.ReactNode;
 };
 
-const shortcodes = { Math };
-
 export const Content = ({ children }: Props) => {
   console.log(children[0]);
   return (
-    <MDXProvider components={shortcodes}>
-      <Styles.Content>
-        <Cover>
-          <ContentImage />
-          <CoverImage />
-        </Cover>
-        <Internal>
-          <InternalTitel>Title: {children[0].props.children}</InternalTitel>
-          <InternalAuthors>
-            <InternalAuthor>
-              <AuthorsPhoto />
-              <AuthorsName>
-                Артемий
-                <br />
-                Михеев
-              </AuthorsName>
-            </InternalAuthor>
-            <InternalAuthor>
-              <AuthorsPhoto />
-              <AuthorsName>
-                Федот
-                <br />
-                Станиславский
-              </AuthorsName>
-            </InternalAuthor>
-            <InternalAuthor>
-              <AuthorsPhoto />
-              <AuthorsName>
-                Константин
-                <br />
-                Константиновский
-              </AuthorsName>
-            </InternalAuthor>
-          </InternalAuthors>
-          {children}
-        </Internal>
-      </Styles.Content>
-    </MDXProvider>
+    <Styles.Content>
+      <Cover>
+        <ContentImage />
+        <CoverImage />
+      </Cover>
+      <Internal>
+        <InternalTitel>Title: {children[0].props.children}</InternalTitel>
+        <InternalAuthors>
+          <InternalAuthor>
+            <AuthorsPhoto />
+            <AuthorsName>
+              Артемий
+              <br />
+              Михеев
+            </AuthorsName>
+          </InternalAuthor>
+          <InternalAuthor>
+            <AuthorsPhoto />
+            <AuthorsName>
+              Федот
+              <br />
+              Станиславский
+            </AuthorsName>
+          </InternalAuthor>
+          <InternalAuthor>
+            <AuthorsPhoto />
+            <AuthorsName>
+              Константин
+              <br />
+              Константиновский
+            </AuthorsName>
+          </InternalAuthor>
+        </InternalAuthors>
+        {children}
+      </Internal>
+    </Styles.Content>
   );
 };
 
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MDXProvider } from '@mdx-js/react';
 
 import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
@@ -9,13 +10,17 @@ type Props = {
   children: React.ReactNode;
 };
 
+const shortcodes = { Math };
+
 const Layout = ({ children }: Props) => {
   return (
-    <Styles.Page>
-      <Header />
-      <Styles.Main>{children}</Styles.Main>
-      <Footer />
-    </Styles.Page>
+    <MDXProvider components={shortcodes}>
+      <Styles.Page>
+        <Header />
+        <Styles.Main>{children}</Styles.Main>
+        <Footer />
+      </Styles.Page>
+    </MDXProvider>
   );
 };
 
