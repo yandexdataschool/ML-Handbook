eee986dd07d24cb2c435877162107ea0b42df81e
taschunina <taschunina@yandex.ru>
2021-08-24 19:34:25 +0300

    feat: footer

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -1,9 +1,20 @@
 import styled from 'styled-components';
 
 export const Footer = styled.footer`
-  margin-top: 0;
-  margin-bottom: 0;
-  height: 100px;
-  font-size: 38px;
-  background-color: blanchedalmond;
+  margin: 0;
+  height: 80px;
+  background-color: #000;
+  opacity: 0.8;
+  display: flex;
+  align-items: flex-end;
+  text-align: right;
+`;
+
+export const FooterText = styled.p`
+  margin-block: 0;
+  margin: auto 23px 11px auto;
+  font-size: 14px;
+  font-weight: 400;
+  opacity: 0.8;
+  color: rgba(114, 114, 114, 1);
 `;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,5 +2,11 @@ import React from 'react';
 import * as Styles from './Footer.styles';
 
 export const Footer = () => {
-  return <Styles.Footer>Congratulations! It is the perfect Footer</Styles.Footer>;
+  return (
+    <Styles.Footer>
+      <Styles.FooterText>
+        &copy; Школа анализа данных, 2007 &ndash; 2021. ООО &laquo;Яндекс&raquo; не оказывает образовательных услуг
+      </Styles.FooterText>
+    </Styles.Footer>
+  );
 };
