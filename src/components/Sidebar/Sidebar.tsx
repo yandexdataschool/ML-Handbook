c923ed76b43d4b3e3a4d7cd09c8222eaa92fa24f
taschunina <taschunina@yandex.ru>
2021-08-28 16:55:44 +0300

    fix: deleted height of the sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Sidebar = styled.section`
   display: none;
   width: 300px;
-  height: 952px;
   color: aqua;
   font-size: 15px;
   border: 1px solid aquamarine;
