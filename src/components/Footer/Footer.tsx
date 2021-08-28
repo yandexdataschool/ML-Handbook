1059de159f9786c59586a858efbfce51977ce27a
taschunina <taschunina@yandex.ru>
2021-08-28 14:24:00 +0300

    fix: footer-footerwrapper, added media, corrected styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,12 +3,12 @@ import * as Styles from './Footer.styles';
 
 export const Footer = () => {
   return (
-    <Styles.FooterWrapper>
-      <Styles.Footer>
+    <Styles.Footer>
+      <Styles.FooterWrapper>
         <Styles.FooterText>
           &copy; Школа анализа данных, 2007 &ndash; 2021. ООО &laquo;Яндекс&raquo; не оказывает образовательных услуг
         </Styles.FooterText>
-      </Styles.Footer>
-    </Styles.FooterWrapper>
+      </Styles.FooterWrapper>
+    </Styles.Footer>
   );
 };
