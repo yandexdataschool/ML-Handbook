df590e7f261452bba83374ffdab9cad0fdf45e94
taschunina <taschunina@yandex.ru>
2021-08-29 17:03:32 +0300

    fix: deleted unnecessary plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,16 +68,5 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        // Footnotes mode (default: true)
-        footnotes: true,
-        // GitHub Flavored Markdown mode (default: true)
-        gfm: true,
-        // Plugins configs
-        plugins: [],
-      },
-    },
   ],
 };
