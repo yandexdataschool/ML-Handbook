517b94a9efa71703171289f624bdf7952d4c6f06
taschunina <taschunina@yandex.ru>
2021-08-29 17:05:50 +0300

    chore: styles of the content

--- src/components/Content/Content.styles.ts
+++ src/components/Content/Content.styles.ts
@@ -4,9 +4,27 @@ import contentImage from './images/rectangle.jpg';
 import coverImage from './images/penguins.png';
 
 export const Content = styled.section`
+  border: 1px solid red;
   max-width: 1140px;
-  margin-left: 13px;
+  @media screen and (min-width: 1140px) {
+    max-width: 820px;
+  }
   @media screen and (min-width: 1440px) {
+    max-width: 1120px;
+  }
+`;
+
+export const Article = styled.section`
+  border: 1px solid black;
+  box-sizing: border-box;
+  max-width: 1140px;
+  @media screen and (min-width: 1140px) {
+    max-width: 820px;
+    padding: 50px 90px 28px 22px;
+    box-shadow: 0px 4px 32px 2px rgba(0, 0, 0, 0.06);
+  }
+  @media screen and (min-width: 1440px) {
+    max-width: 1120px;
     padding-left: 44px;
     padding-right: 181px;
   }
@@ -14,67 +32,106 @@ export const Content = styled.section`
 
 export const Cover = styled.div`
   display: none;
-  @media screen and (min-width: 1440px) {
-    margin-top: 50px;
-    margin-left: -7px;
+  @media screen and (min-width: 1140px) {
     display: block;
-    width: 911px;
-    height: 408px;
+    max-width: 728px;
+    max-height: 408px;
+  }
+  @media screen and (min-width: 1440px) {
+    max-width: 911px;
+    max-height: 408px;
   }
 `;
 
-export const ContentImage = styled.div`
-  width: 911px;
-  height: 356px;
-  margin: 0;
+export const ArticleImage = styled.div`
   background-repeat: no-repeat;
   background-image: url(${contentImage});
+  border-radius: 12px;
+  @media screen and (min-width: 1140px) {
+    width: 728px;
+    height: 283px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 911px;
+    height: 356px;
+  }
 `;
 
 export const CoverImage = styled.div`
-  width: 438px;
-  height: 137px;
-  margin-top: -85px;
-  margin-left: 461px;
   z-index: 2;
   background-repeat: no-repeat;
   background-image: url(${coverImage});
+  background-size: contain;
+  @media screen and (min-width: 1140px) {
+    width: 350px;
+    height: 109px;
+    margin-top: -75px;
+    margin-left: 360px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 438px;
+    height: 137px;
+    margin-top: -85px;
+    margin-left: 461px;
+  }
 `;
 
 export const Internal = styled.div`
   display: block;
   @media screen and (min-width: 1440px) {
-    width: 900px;
+    max-width: 1120px;
   }
 `;
 
-export const InternalTitel = styled.p`
-  font-size: 24px;
-  font-weight: 500;
-  color: #000;
-  @media screen and (min-width: 1440px) {
-    display: block;
-    font-size: 44px;
-    font-weight: 700;
-  }
-`;
-
-export const InternalAuthors = styled.div`
-  display: block;
-  width: 263px;
-  margin: 0 auto;
-  @media screen and (min-width: 1440px) {
-    display: flex;
-    margin-left: 117px;
-    width: 789px;
-    height: 107px;
-  }
-`;
-
-export const InternalAuthor = styled.div`
+export const ArticleTitle = styled.div`
+  font-size: 18px;
+  color: rgba(0, 0, 0, 1);
+  min-width: 320px;
+  min-height: 47px;
   display: flex;
-  width: 263px;
-  height: 107px;
+  @media screen and (min-width: 1140px) {
+    font-size: 44px;
+  }
+`;
+
+export const TitleNumber = styled.p`
+  font-weight: 400;
+  margin-right: 6px;
+  min-width: 20px;
+`;
+
+export const TitleText = styled.p`
+  font-weight: 700;
+  min-width: 280px;
+`;
+
+export const ArticleAuthors = styled.div`
+  display: block;
+  min-width: 227px;
+  margin-left: 30px;
+  @media screen and (min-width: 1440px) {
+    margin-left: 117px;
+  }
+`;
+
+export const AuthorsList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 227px);
+`;
+
+export const AuthorsTitle = styled.p`
+  min-width: 228px;
+  min-height: 36px;
+  font-family: inherit;
+  font-weight: 400;
+  font-style: italic;
+  font-size: 14px;
+`;
+
+export const ArticleAuthor = styled.div`
+  display: flex;
+  min-width: 227px;
+  min-height: 107px;
 `;
 
 export const AuthorsPhoto = styled.div`
@@ -92,5 +149,5 @@ export const AuthorsName = styled.p`
   font-size: 16px;
   line-height: 20px;
   text-decoration-line: underline;
-  color: #3b4856;
+  color: rgba(59, 72, 86, 1);
 `;
--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,60 +1,61 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
+
+import { Finisher } from '../Finisher/Finisher';
+
 import * as Styles from './Content.styles';
-import {
-  Cover,
-  ContentImage,
-  CoverImage,
-  Internal,
-  InternalTitel,
-  InternalAuthors,
-  InternalAuthor,
-  AuthorsPhoto,
-  AuthorsName,
-} from './Content.styles';
 
 type Props = {
   children: React.ReactNode;
 };
 
+const shortcodes = { Math };
+
 export const Content = ({ children }: Props) => {
   return (
     <MDXProvider components={shortcodes}>
       <Styles.Content>
-        <Cover>
-          <ContentImage />
-          <CoverImage />
-        </Cover>
-        <Internal>
-          <InternalTitel>Title: </InternalTitel>
-          <InternalAuthors>
-            <InternalAuthor>
-              <AuthorsPhoto />
-              <AuthorsName>
-                Артемий
-                <br />
-                Михеев
-              </AuthorsName>
-            </InternalAuthor>
-            <InternalAuthor>
-              <AuthorsPhoto />
-              <AuthorsName>
-                Федот
-                <br />
-                Станиславский
-              </AuthorsName>
-            </InternalAuthor>
-            <InternalAuthor>
-              <AuthorsPhoto />
-              <AuthorsName>
-                Константин
-                <br />
-                Константиновский
-              </AuthorsName>
-            </InternalAuthor>
-          </InternalAuthors>
-          {children}
-        </Internal>
+        <Styles.Article>
+          <Styles.Cover>
+            <Styles.ArticleImage />
+            <Styles.CoverImage />
+          </Styles.Cover>
+          <Styles.ArticleTitle>
+            <Styles.TitleNumber>&sect; 4.</Styles.TitleNumber>
+            <Styles.TitleText>Вероятностный подход в ML</Styles.TitleText>
+          </Styles.ArticleTitle>
+          <Styles.ArticleAuthors>
+            <Styles.AuthorsTitle>Авторы статьи:</Styles.AuthorsTitle>
+            <Styles.AuthorsList>
+              <Styles.ArticleAuthor>
+                <Styles.AuthorsPhoto />
+                <Styles.AuthorsName>
+                  Артемий
+                  <br />
+                  Михеев
+                </Styles.AuthorsName>
+              </Styles.ArticleAuthor>
+              <Styles.ArticleAuthor>
+                <Styles.AuthorsPhoto />
+                <Styles.AuthorsName>
+                  Федот
+                  <br />
+                  Станиславский
+                </Styles.AuthorsName>
+              </Styles.ArticleAuthor>
+              <Styles.ArticleAuthor>
+                <Styles.AuthorsPhoto />
+                <Styles.AuthorsName>
+                  Константин
+                  <br />
+                  Константиновский
+                </Styles.AuthorsName>
+              </Styles.ArticleAuthor>
+            </Styles.AuthorsList>
+          </Styles.ArticleAuthors>
+          <Styles.Internal>{children}</Styles.Internal>
+        </Styles.Article>
+        <Finisher>Далее</Finisher>
       </Styles.Content>
     </MDXProvider>
   );
