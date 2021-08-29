a99342255d3cc29b58d7e4bbbe4734c012ef5bf3
taschunina <taschunina@yandex.ru>
2021-08-29 12:43:43 +0300

    chore: add media, corr rgba

--- src/components/Layout/Layout.styles.ts
+++ src/components/Layout/Layout.styles.ts
@@ -3,17 +3,27 @@ import '../../typography.css';
 
 export const Page = styled.div`
   font-family: 'YS Text', georgia, serif;
-  max-width: 1440px;
-  margin: 0 auto;
-  background-color: #fff;
-  color: rgba(51, 51, 102, 1);
   font-style: normal;
   font-weight: normal;
+  margin: 0 auto;
+  background-color: rgba(255, 255, 255, 1);
+  color: rgba(0, 0, 0, 1);
+  border: 1px solid green;
 `;
 
 export const Main = styled.main`
-  margin: 4px auto 0;
+  border: 1px solid blue;
+  font-family: inherit;
+  color: inherit;
   font-size: 18px;
   line-height: 28px;
-  display: flex;
+  box-sizing: border-box;
+  padding: 10px;
+  @media screen and (min-width: 1140px) {
+    box-sizing: content-box;
+    padding: 0;
+    max-width: 1440px;
+    margin: 4px auto 0;
+    display: flex;
+  } ;
 `;
