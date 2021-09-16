21637732258abb6729f501454138c1b90f9a7131
Khizri Makhmudov <kh.makhmudov@me.com>
2021-09-16 13:25:59 +0300

    fix: add Math and Link prop types

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,6 +6,7 @@ import { Footer } from '../Footer/Footer';
 
 import MathBlock from '../MathBlock/MathBlock';
 import MathInline from '../MathInline/MathInline';
+import Link from '../Link/Link';
 
 import * as Styles from './Layout.styles';
 
@@ -13,9 +14,19 @@ type Props = {
   children: React.ReactNode;
 };
 
+type MathProps = {
+  children: React.ReactNode;
+};
+
+type LinkProps = {
+  href: string;
+  children: React.ReactNode;
+};
+
 const shortcodes = {
-  div: ({ children }: Props) => <MathBlock>{children}</MathBlock>,
-  span: ({ children }: Props) => <MathInline>{children}</MathInline>,
+  div: ({ children }: MathProps) => <MathBlock>{children}</MathBlock>,
+  span: ({ children }: MathProps) => <MathInline>{children}</MathInline>,
+  a: ({ href, children }: LinkProps) => <Link href={href}>{children}</Link>,
 };
 
 const Layout = ({ children }: Props) => {
