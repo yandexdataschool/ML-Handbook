24d859e74e4ef234b00e9c5f87e7939c85c476fe
taschunina <taschunina@yandex.ru>
2021-09-18 22:26:49 +0300

    chore: added dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: ['./src/pages/**/*.{tsx,mdx}', './src/components/**/*.{tsx,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    theme: {
+      fontSize: {
+        xs: '0.75rem',
+        sm: '0.875rem',
+        base: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
