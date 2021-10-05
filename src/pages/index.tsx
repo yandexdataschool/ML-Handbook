d27beb61f57473dafaad97230103b7ed8f54b282
taschunina <taschunina@yandex.ru>
2021-10-05 07:32:51 +0200

    chore: edited mainpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 import * as React from 'react';
 import { Link, graphql } from 'gatsby';
-import Layout from '../components/Layout/Layout';
+import MainLayout from '../components/MainLayout/MainLayout';
 
 const MainPage = ({ data }) => {
   return (
-    <Layout>
+    <MainLayout>
       <p>
         Перед вами учебник по машинному обучению. Он состоит из двух частей — основного курса теории, который надлежит
         читать подряд, и набора статей отвечающих на вопросы возникающие в реальной практике аналитика или ML-инженера.
@@ -17,7 +17,7 @@ const MainPage = ({ data }) => {
           </h2>
         </article>
       ))}
-    </Layout>
+    </MainLayout>
   );
 };
 
