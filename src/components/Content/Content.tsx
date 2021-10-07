ea4322ed400939d2f02dd7a49079f9a27ad8050a
taschunina <taschunina@yandex.ru>
2021-10-07 15:35:09 +0200

    feat: retrieving titles and authors via frontmatter

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+// import { graphql } from 'gatsby';
 import { MDXProvider } from '@mdx-js/react';
 
+import { graphql } from 'gatsby';
 import { TableOfContents } from '../TableOfContents/TableOfContents';
 import { Finisher } from '../Finisher/Finisher';
 
@@ -12,53 +15,74 @@ type Props = {
 
 const shortcodes = { Math };
 
-export const Content = ({ children }: Props) => {
+const Content = ({ children }: Props, { data }) => {
   return (
     <MDXProvider components={shortcodes}>
-      <Styles.Content>
-        <Styles.Article>
-          <Styles.Cover>
-            <Styles.ArticleImage />
-            <Styles.CoverImage />
-          </Styles.Cover>
-          <Styles.ArticleTitle>
-            <Styles.TitleNumber>&sect; 4.</Styles.TitleNumber>
-            <Styles.TitleText>Вероятностный подход в ML</Styles.TitleText>
-          </Styles.ArticleTitle>
-          <Styles.ArticleAuthors>
-            <Styles.AuthorsTitle>Авторы статьи:</Styles.AuthorsTitle>
-            <Styles.AuthorsList>
-              <Styles.ArticleAuthor>
-                <Styles.AuthorsPhoto />
-                <Styles.AuthorsName>
-                  Артемий
-                  <br />
-                  Михеев
-                </Styles.AuthorsName>
-              </Styles.ArticleAuthor>
-              <Styles.ArticleAuthor>
-                <Styles.AuthorsPhoto />
-                <Styles.AuthorsName>
-                  Федот
-                  <br />
-                  Станиславский
-                </Styles.AuthorsName>
-              </Styles.ArticleAuthor>
-              <Styles.ArticleAuthor>
-                <Styles.AuthorsPhoto />
-                <Styles.AuthorsName>
-                  Константин
-                  <br />
-                  Константиновский
-                </Styles.AuthorsName>
-              </Styles.ArticleAuthor>
-            </Styles.AuthorsList>
-          </Styles.ArticleAuthors>
-          <TableOfContents />
-          <Styles.Internal>{children}</Styles.Internal>
-        </Styles.Article>
-        <Finisher>Далее</Finisher>
-      </Styles.Content>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <Styles.Content>
+            <Styles.Article>
+              <Styles.Cover>
+                <Styles.ArticleImage />
+                <Styles.CoverImage />
+              </Styles.Cover>
+              <Styles.ArticleTitle>
+                <Styles.TitleNumber />
+                <Helmet>
+                  <Styles.TitleText>{node.frontmatter.title}</Styles.TitleText>
+                </Helmet>
+              </Styles.ArticleTitle>
+              <Styles.ArticleAuthors>
+                <Styles.AuthorsTitle>Авторы статьи:</Styles.AuthorsTitle>
+                <Styles.AuthorsList>
+                  <Styles.ArticleAuthor>
+                    <Styles.AuthorsPhoto />
+                    <Styles.AuthorsName>{node.frontmatter.author}</Styles.AuthorsName>
+                  </Styles.ArticleAuthor>
+                  <Styles.ArticleAuthor>
+                    <Styles.AuthorsPhoto />
+                    <Styles.AuthorsName>
+                      Федот
+                      <br />
+                      Станиславский
+                    </Styles.AuthorsName>
+                  </Styles.ArticleAuthor>
+                  <Styles.ArticleAuthor>
+                    <Styles.AuthorsPhoto />
+                    <Styles.AuthorsName>
+                      Константин
+                      <br />
+                      Константиновский
+                    </Styles.AuthorsName>
+                  </Styles.ArticleAuthor>
+                </Styles.AuthorsList>
+              </Styles.ArticleAuthors>
+              <TableOfContents />
+              <Styles.Internal>{children}</Styles.Internal>
+            </Styles.Article>
+            <Finisher>Далее</Finisher>
+          </Styles.Content>
+        </article>
+      ))}
     </MDXProvider>
   );
 };
+
+export const query = graphql`
+  query {
+    allMdx {
+      nodes {
+        frontmatter {
+          author
+          tg
+          title
+          toc
+        }
+        id
+        slug
+      }
+    }
+  }
+`;
+
+export default Content;
