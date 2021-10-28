95ff9a12c2b31f9b0ebe702f864266aadc26ca30
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-10-28 18:06:23 +0400

    feat: add index page with toc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,105 @@
 import Head from "next/head";
 import { Layout } from "@/components/Layout/Layout";
+import Link from "next/link";
 
-const Home = () => {
+type Chapter = {
+  title: string;
+  slug: string;
+};
+
+type Section = {
+  title: string;
+  slug?: string;
+  chapters?: Chapter[];
+};
+
+const Chapter = ({ title, slug }: Chapter) => {
+  return (
+    <li>
+      <Link href={slug}>{title}</Link>
+    </li>
+  );
+};
+
+const Section = ({ title, slug, chapters }: Section) => {
+  if (slug) {
+    return (
+      <li>
+        <Link href={slug}>{title}</Link>
+      </li>
+    );
+  }
+
+  return (
+    <>
+      <li>
+        <p>{title}</p>
+      </li>
+      <ol type="i" className="ml-4">
+        {chapters.map((chapter) => (
+          <Chapter key={chapter.slug} title={chapter.title} slug={chapter.slug} />
+        ))}
+      </ol>
+    </>
+  );
+};
+
+const Home = ({ sections }) => {
+  console.log(sections);
   return (
     <Layout>
       <Head>
         <title>Main page title, if you want to set title or other meta tags</title>
       </Head>
-      <main>
-        <h1>Hello, world!</h1>
+      <main className="container mx-auto">
+        <div className="prose">
+          <h1>Школа анализа данных</h1>
+          <ol>
+            {sections.map((section) => (
+              <Section key={section.title} slug={section.slug} title={section.title} chapters={section.chapters} />
+            ))}
+          </ol>
+        </div>
       </main>
     </Layout>
   );
+};
+
+export const getStaticProps = () => {
+  const sections = [
+    {
+      title: "Введение",
+      slug: "intro",
+      description: "",
+    },
+    {
+      title: "Классическое обучение с учителем",
+      chapters: [
+        {
+          title: "Линейные модели",
+          slug: "linear_models/intro",
+        },
+        {
+          title: "Метрические алгоритмы ",
+          slug: "metric_based/intro",
+        },
+        {
+          title: "Деревья решений",
+          slug: "decision_tree/intro",
+        },
+      ],
+    },
+    {
+      title: "Оценка качества моделей",
+      slug: "model_evaluation/intro",
+    },
+  ];
+
+  return {
+    props: {
+      sections,
+    },
+  };
 };
 
 export default Home;
