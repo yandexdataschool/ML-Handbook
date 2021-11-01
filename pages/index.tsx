ec1d7c7a067104e5b51550301499c94052a937e4
Khizri Makhmudov <kh.makhmudov@me.com>
2021-11-01 11:20:03 +0300

    feat: add new components to the main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,19 @@
 import Head from "next/head";
 import { Layout } from "@/components/Layout/Layout";
-import Link from "next/link";
-
-type Chapter = {
-  title: string;
-  slug: string;
-};
-
-type Section = {
-  title: string;
-  slug?: string;
-  chapters?: Chapter[];
-};
-
-const Chapter = ({ title, slug }: Chapter) => {
-  return (
-    <li>
-      <Link href={slug}>{title}</Link>
-    </li>
-  );
-};
-
-const Section = ({ title, slug, chapters }: Section) => {
-  if (slug) {
-    return (
-      <li>
-        <Link href={slug}>{title}</Link>
-      </li>
-    );
-  }
-
-  return (
-    <>
-      <li>
-        <p>{title}</p>
-      </li>
-      <ol type="i" className="ml-4">
-        {chapters.map((chapter) => (
-          <Chapter key={chapter.slug} title={chapter.title} slug={chapter.slug} />
-        ))}
-      </ol>
-    </>
-  );
-};
+import { Header } from "@/components/Header/Header";
+import { Main } from "@/components/Main/Main";
+import { Footer } from "@/components/Footer/Footer";
 
 const Home = ({ sections }) => {
   console.log(sections);
   return (
     <Layout>
       <Head>
-        <title>Main page title, if you want to set title or other meta tags</title>
+        <title>Курс по теории машинного обучения от Школы Анализа Данных</title>
       </Head>
-      <main className="container mx-auto">
-        <div className="prose">
-          <h1>Школа анализа данных</h1>
-          <ol>
-            {sections.map((section) => (
-              <Section key={section.title} slug={section.slug} title={section.title} chapters={section.chapters} />
-            ))}
-          </ol>
-        </div>
-      </main>
+      <Header />
+      <Main sections={sections} />
+      <Footer />
     </Layout>
   );
 };
