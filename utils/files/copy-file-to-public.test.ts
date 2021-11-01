49d7d2885532fad647eebff6846e1b349d2eb9f8
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-11-01 13:39:39 +0400

    feat: copy files to additional public folder

--- utils/files/copy-file-to-public.test.ts
+++ utils/files/copy-file-to-public.test.ts
@@ -5,7 +5,11 @@ import { copyFileToPublic } from "./copy-file-to-public";
 beforeEach(function () {
   mock({
     public: mock.directory({
-      items: {},
+      items: {
+        "additional-folder": mock.directory({
+          items: {},
+        }),
+      },
     }),
     "folder-name/file-name.ext": mock.file({
       content: "content",
@@ -13,10 +17,15 @@ beforeEach(function () {
   });
 });
 describe("copyFileToPublic", () => {
-  it("should pass correctly", () => {
+  it("should copy file correctly", () => {
     copyFileToPublic("folder-name/file-name.ext");
     expect(fs.existsSync("public/file-name.ext")).toBeTruthy();
     expect(String(fs.readFileSync("public/file-name.ext"))).toBe("content");
+  });
+  it("should copy file in additional folder", () => {
+    copyFileToPublic("folder-name/file-name.ext", "additional-folder");
+    expect(fs.existsSync("public/additional-folder/file-name.ext")).toBeTruthy();
+    expect(String(fs.readFileSync("public/additional-folder/file-name.ext"))).toBe("content");
   });
 });
 afterEach(mock.restore);
--- utils/files/copy-file-to-public.ts
+++ utils/files/copy-file-to-public.ts
@@ -7,9 +7,9 @@ const last = (array) => {
   return length ? array[length - 1] : undefined;
 };
 
-export const copyFileToPublic = (filePath: string): void => {
+export const copyFileToPublic = (filePath: string, additionalPath: string = ""): void => {
   const fullFilePath = path.join(process.cwd(), filePath);
-  const publicPath = path.join(process.cwd(), "public");
+  const publicPath = path.join(process.cwd(), "public", additionalPath);
   const fileName = last(filePath.split("/"));
   const publicFilePath = path.join(publicPath, fileName);
 
