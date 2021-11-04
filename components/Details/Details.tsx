bc3c32744af78b9114fe41450bfcac7dc944089e
taschunina <taschunina@yandex.ru>
2021-11-04 19:29:33 +0300

    fix: added border for the details

--- components/Details/Details.tsx
+++ components/Details/Details.tsx
@@ -1,25 +1,5 @@
-import * as Styles from './Details.styles';
 
 export const Details = ({ children }) => {
-  return <Styles.Details>{children}</Styles.Details>;
+  return <details>{children}</details>;
 };
-
-
-// import React from 'react';
-
-// import * as Styles from './Details.styles';
-
-// export type SummaryProps = {
-//   label: string;
-//   children: React.ReactNode;
-// }
-
-// export const Details = ({ label, children }: SummaryProps) => {
-//   return (
-//     <Styles.Details>
-//       <Styles.Summary>{label}</Styles.Summary>
-//       <Styles.Content>{children}</Styles.Content>
-//     </Styles.Details>
-//   );
-// };
 
