ebb5db931146cb288d4c2f248148860b6c0a6a89
taschunina <taschunina@yandex.ru>
2021-11-05 13:26:14 +0300

    fix: changed main params
    deleted max-width max-w-5xl
    added display flex

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -16,6 +16,7 @@ import { IFrame } from "@/components/IFrame/IFrame";
 import { Content } from "@/components/Content/Content";
 import { HeaderArticle } from "@/components/HeaderArticle/HeaderArticle";
 import { Footer } from "@/components/Footer/Footer";
+import { Sidebar } from "@/components/Sidebar/Sidebar";
 
 
 /* Utils */
@@ -51,11 +52,14 @@ export default function ArticlePage({ code, frontMatter, tableOfContents }: Prop
         <meta name="description" content={frontMatter.description} />
       </Head>
       <HeaderArticle />
-      <main className="container mx-auto max-w-5xl">
-        <Content>
-          {/*@ts-ignore*/}
-          <Component components={components} />
-        </Content>
+      <main className="container mx-auto flex">
+      
+          <Sidebar />
+          <Content>
+            {/*@ts-ignore*/}
+            <Component components={components} />
+          </Content>
+       
       </main>
       <Footer />
     </Layout>
