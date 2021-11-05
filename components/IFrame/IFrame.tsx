891ec9d58e8bb4894ee3c3e919472aa209de213e
Khizri Makhmudov <kh.makhmudov@me.com>
2021-11-05 14:41:41 +0300

    feat: add some styling && auto resizing to IFrame

--- components/IFrame/IFrame.tsx
+++ components/IFrame/IFrame.tsx
@@ -1,5 +1,22 @@
+import React from "react";
 const INTERACTIVE_FOLDER = "interactive";
 
 export const IFrame = ({ src }) => {
-  return <iframe src={`/${INTERACTIVE_FOLDER}${src.slice(1)}`} frameBorder="0" />;
+  const [height, setHeight] = React.useState(0);
+  const ref = React.useRef(null);
+
+  React.useEffect(() => {
+    setHeight(ref.current.contentWindow.document.body.scrollHeight);
+  }, []);
+
+  return (
+    <iframe
+      ref={ref}
+      className="w-full my-5"
+      scrolling="no"
+      height={height}
+      src={`/${INTERACTIVE_FOLDER}${src.slice(1)}`}
+      frameBorder="0"
+    />
+  );
 };
